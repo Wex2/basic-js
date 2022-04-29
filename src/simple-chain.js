7a697ed5d197bf001fed7920c26a9e0e2b1187e7
Wex2 <gatsqp@gmail.com>
2022-04-29 22:17:25 +0300

    feat: add simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,37 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    chain: [],
+    getLength() {
+        return this.chain.length
+    },
+    addLink(value) {
+        this.chain.push(`( ${value} )`)
+        return this
+    },
+    removeLink(position) {
+        if (this.chain[position - 1] === undefined) {
+            this.chain = []
+            throw new Error("You can't remove incorrect link!")
+        }
+        this.chain.splice(position - 1, 1)
+        return this
+    },
+    reverseChain() {
+        this.chain.reverse()
+        return this
+    },
+    finishChain() {
+        let finish = this.chain.join('~~')
+        this.chain = []
+        return finish
+    }
 };
 
 module.exports = {
-  chainMaker
+    chainMaker
 };
